53b3217964d8488dc6d4fa002b7d8a70571a4164
mmanvelyanknuua <mmanvelyan@knu.ua>
2022-10-12 23:02:35 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+abstract class geographicObj
+{
+    double x, y;
+    string name;
+    string description;
+    public geographicObj() { }
+    public geographicObj (double x, double y, string name, string description)
+    {
+        this.x = x; this.y = y; this.name = name; this.description = description;
+    }
+}
+
+class river : geographicObj
+{
+    double speed;
+    double length;
+
+}
+
+class mountain : geographicObj
+{
+    double highestPoint;
+
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.VisualBasic;
+
+interface IGeographicObj
+{
+    public void information(double x, double y, string name, string description);
+}
+class river : IGeographicObj
+{
+    double x;
+    double y;
+    string name;
+    string description;
+    double speed;
+    double length;
+    public void information(double x, double y, string name, string description)
+        {
+            this.x = x;
+            this.y = y;
+            this.name = name;
+            this.description = description;
+        }
+}
+
+class mountain : IGeographicObj
+{
+    double x;
+    double y;
+    string name;
+    string description;
+    double highestPoint;
+    public void information(double x, double y, string name, string description)
+    {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.description = description;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
+
